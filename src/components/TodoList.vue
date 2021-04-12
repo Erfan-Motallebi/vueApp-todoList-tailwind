f07c595c24b89a4b6f67e85a56c8878f7f5c1c56
Erfan <erfan.fulldevs@gmail.com>
2021-04-12 08:38:06 +0430

    made new stuff and examples using styles

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
+  <div class="w-full flex flex-col">
+    <section class="w-1/2 m-auto">
+      <h1 class="text-center rounded shadow-lg py-3">
+        Todo Counter : {{ todoCounter }}
+      </h1>
+    </section>
     <section>
-      <h1>Todo Counter : {{ todoCounter }}</h1>
+      <p
+        class="w-4/6 m-auto border rounded text-left py-3 mt-5"
+        v-for="todo in todos"
+        :key="todo.id"
+      >
+        {{ todo.job }}
+      </p>
     </section>
   </div>
 </template>
@@ -11,6 +22,11 @@ export default {
   data() {
     return {
       todoCounter: 0,
+      todos: [
+        { id: 0, job: "making a new vue-based application" },
+        { id: 1, job: "make here a better world" },
+        { id: 2, job: "going to the gym" },
+      ],
     };
   },
 };
