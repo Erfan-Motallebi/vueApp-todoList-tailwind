b7dde8640e03aafa788fd7557f56d72ee44e9d61
Erfan <erfan.fulldevs@gmail.com>
2021-04-12 08:48:26 +0430

    made a button and form with an event submit being prevented the default action as a link

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,13 +7,18 @@
     </section>
     <section>
       <p
-        class="w-4/6 m-auto border rounded text-left py-3 mt-5"
+        class="w-4/6 m-auto border rounded text-left p-3 mt-5 relative"
         v-for="todo in todos"
         :key="todo.id"
       >
         {{ todo.job }}
+        <button class="absolute right-0">remove</button>
       </p>
     </section>
+    <form @submit.prevent="" class="m-auto my-3">
+      <input type="text" class="border rounded" /> <br />
+      <input type="submit" value="ADD" class="w-1/2 mx-16" />
+    </form>
   </div>
 </template>
 
