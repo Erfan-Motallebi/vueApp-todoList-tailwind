36c4d62e162bb93e6e5e9244c85b7bc1a05f3031
Erfan <erfan.fulldevs@gmail.com>
2021-04-12 10:02:22 +0430

    checked the insert and remove button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,14 +12,17 @@
         :key="todo.id"
       >
         {{ todo.job }}
-        <button class="absolute right-2 text-red-500">remove</button>
+        <button class="absolute right-2 text-red-500" @click="remove(todo.id)">
+          remove
+        </button>
       </p>
     </section>
-    <form @submit.prevent="" class="m-auto my-3">
+    <form @submit.prevent="addTask()" class="m-auto my-3">
       <input
         type="text"
         class="border border-gray-400 rounded placeholder-red-300 focus:placeholder-yellow-80 px-2"
         placeholder="Your Task"
+        v-model="newTask"
       />
       <br />
       <input
@@ -36,12 +39,28 @@ export default {
   data() {
     return {
       todoCounter: 0,
+      newTask: "",
       todos: [
         { id: 0, job: "making a new vue-based application" },
         { id: 1, job: "make here a better world" },
         { id: 2, job: "going to the gym" },
       ],
     };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask == "") {
+        console.error("error founded");
+      } else {
+        this.todos.push({
+          id: new Date().getTime().toString(),
+          job: this.newTask,
+        });
+      }
+    },
+    remove(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
   },
 };
 </script>
